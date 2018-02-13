411d680909784fcc11556a83fbeb79086afbac21
Igor Eskin <github email address>
2018-02-13 16:59:21 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let body = document.querySelector('body');
+  let index = 0;
 
-}
+  body.addEventListener('keydown', function (event) {
+    // console.log(event);
+    let key = parseInt(event.detail || event.which);
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("You've cracked the code!");
+        index = 0;
+      }
+    } else {
+        index = 0;
+      }
+    });
+  }
+
+  init();
